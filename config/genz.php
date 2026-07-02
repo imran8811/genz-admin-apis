d6e1d705dbffde6d386ac605e338348c77fc3a41
unknown <M Imran>
2026-07-02 13:02:15 +0500

    changes

--- config/genz.php
+++ config/genz.php
@@ -3,5 +3,5 @@
 return [
     // Public base URL used to build absolute menu image URLs in the public feed.
     // Images live at deterministic paths: {public_url}/menu/{category}/{item}.webp
-    'public_url' => rtrim(env('ADMIN_PUBLIC_URL', env('APP_URL', 'http://localhost:8002')), '/'),
+    'public_url' => rtrim(env('ADMIN_PUBLIC_URL', env('APP_URL', 'https://api.admin.genzfoods.pk')), '/'),
 ];
